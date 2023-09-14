c0aed3f09dea7f0a0b16f91ba169a5d68327c70b
Kyle Crawford <kcraw217@gmail.com>
2023-09-13 23:38:36 -0700

    fix margin

--- pages/components/Navbar.tsx
+++ pages/components/Navbar.tsx
@@ -6,7 +6,7 @@ import { motion } from 'framer-motion'
 const Navbar = () => {
   return (
     <div className="w-full shadow-navbarShadow h-20 lg:h-[12vh] sticky top-0 z-50 bg-bodyColor px-4">
-      <div className="max-w-container h-full ms-auto py-1 font-titleFont flex items-center justify-between">
+      <div className="max-w-container h-full mx-auto py-1 font-titleFont flex items-center justify-between">
         <motion.div
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
