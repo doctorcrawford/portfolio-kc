787498871fcc86ce7a6d04697115e473c34bb7e5
Kyle Crawford <kcraw217@gmail.com>
2023-09-14 14:57:39 -0700

    fix id

--- pages/components/Projects.tsx
+++ pages/components/Projects.tsx
@@ -7,7 +7,7 @@ import { RxOpenInNewWindow } from 'react-icons/rx';
 
 const Projects = () => {
   return (
-    <section id='project' className='max-w-container mx-auto lgl:px-20 py-24'>
+    <section id='projects' className='max-w-container mx-auto lgl:px-20 py-24'>
       <SectionTitle title='Some Things I Have Built' titleNo='02.' />
       <div className='w-full flex flex-col items-center justify-between gap-28 mt-10'>
         {/* Project one */}
