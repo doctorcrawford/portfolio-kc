41043062a3ff2b59a362bdf2649c270c55a73947
Kyle Crawford <kcraw217@gmail.com>
2023-09-13 23:54:17 -0700

    add description p and project button

--- pages/components/Banner.tsx
+++ pages/components/Banner.tsx
@@ -24,6 +24,22 @@ const Banner = () => {
           I like to build things.
         </span>
       </motion.h1>
+      <motion.p
+        initial={{ y: 10, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.5, delay: 0.8 }}
+        className='text-base md:max-w-[650px] text-textDark font-medium'
+      >
+        Full stack software developer, with a foundation in JavaScript, Typescript, React, and C#/.NET. I enjoy playing and producing music, working with my hands and exploring the great outdoors.
+      </motion.p>
+      <motion.button
+        initial={{ y: 10, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.5, delay: 0.9 }}
+        className='w-52 h-14 text-sm font-titleFont border border-textGreen rounded-md text-textGreen tracking-wide hover:bg-hoverColor duration-300'
+      >
+        Check out my projects!
+      </motion.button>
     </section>
   );
 };
