216f7067133f0dbb047ea9e24744e6f38a047153
Kyle Crawford <kcraw217@gmail.com>
2023-09-14 15:12:36 -0700

    change section titles to triangles

--- pages/components/Contact.tsx
+++ pages/components/Contact.tsx
@@ -1,3 +1,4 @@
+import { TbTriangleInverted } from 'react-icons/tb'
 
 const Contact = () => {
   return (
@@ -6,7 +7,8 @@ const Contact = () => {
       className="max-w-contentContainer mx-auto py-10 xl:py-32 flex flex-col gap-4 items-center justify-center"
     >
       <p className="font-titleFont text-lg text-textYellow font-semibold flex items-center tracking-wide">
-        03. What&lsquo;s Next?
+        <TbTriangleInverted /><TbTriangleInverted />
+        <TbTriangleInverted /><TbTriangleInverted />
       </p>
       <h2 className="font-titleFont text-5xl font-semibold">Get In Touch</h2>
       <p className="max-w-[600px] text-center text-textDark">
--- pages/components/SectionTitle.tsx
+++ pages/components/SectionTitle.tsx
@@ -1,12 +1,13 @@
+import { TbTriangleInverted } from 'react-icons/tb';
+
 interface Props {
   title: string;
-  titleNo: string;
 }
 
-const SectionTitle = ({ title, titleNo }: Props) => {
+const SectionTitle = ({ title }: Props) => {
   return (
     <h2 className='font-titleFont text-2xl font-semibold flex items-center'>
-      <span className='text-base md:text-lg text-textYellow mr-2'>{titleNo}</span>
+      <span className='text-base md:text-lg text-textYellow mr-2'><TbTriangleInverted /></span>
       {title}
       <span className='hidden md:inline-flex md:w-60 lgl:w-72 h-[.5px] bg-gray-700 ml-6'>
 
