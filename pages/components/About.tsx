0e0a4758d96aaf9a20e0622cfbc560fc7b82e7a8
Kyle Crawford <kcraw217@gmail.com>
2023-09-14 00:14:42 -0700

    add section title

---
+++ pages/components/About.tsx
@@ -0,0 +1,14 @@
+import SectionTitle from "./SectionTitle"
+
+const About = () => {
+  return (
+    <section
+      id="about"
+      className="max-w-containerSmall mx-auto py-10 lgl:py-32 flex flex-col gap-8"
+    >
+      <SectionTitle title="About Me" titleNo="0.1"/>
+    </section>
+  )
+}
+
+export default About;
