5289e504863517d465bf4daf1149de4058d971bf
Kyle Crawford <kcraw217@gmail.com>
2023-09-14 14:47:04 -0700

    add scrollbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ import Footer from "./components/Footer";
 
 export default function Home() {
   return (
-    <main className="w-full h-screen font-bodyFont bg-bodyColor text-textLight overflow-x-hidden overflow-y-scroll">
+    <main className="w-full h-screen font-bodyFont bg-bodyColor text-textLight overflow-x-hidden overflow-y-scroll scrollbar scrollbar-track-textDark/20 scrollbar-thumb-textDark/60">
       <Navbar />
       <div className="w-full h-[88vh] flex flex-row xl:flex items-center gap-20 justify-between">
         <motion.div
