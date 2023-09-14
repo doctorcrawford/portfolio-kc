217b00d391206b738abd6b9d221cdc1ecc379de7
Kyle Crawford <kcraw217@gmail.com>
2023-09-14 08:58:18 -0700

    add synesthesia project

--- pages/components/Projects.tsx
+++ pages/components/Projects.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import Image from 'next/image'
+import synesthesia from '@/public/assets/images/synesthesia.png';
 import SectionTitle from './SectionTitle'
 
 const Projects = () => {
@@ -7,8 +8,28 @@ const Projects = () => {
       <SectionTitle title='Some Things I Have Built' titleNo='02' />
       {/* Proejct one */}
       <div className='w-full flex flex-col items-center justify-center gap-28 mt-10'>
-        <div></div>
-        <div></div>
+        <div className='flex flex-col xl:flex-row gap-6'>
+          <a
+            className='w-full xl:w-1/2 h-auto relative group'
+            href='https://doctorcrawford.github.io/synesthesia/'
+            target='_blank'
+          >
+            <Image
+              className='w-full h-full object-contain'
+              src={synesthesia}
+              alt='synesthesia'
+            />
+          </a>
+          <div className='w-full xl:w-1/2 flex flex-col gap-6 lgl:justify-between items-end text-right xl:-ml-16 z-10'>
+            <p className='font-titleFont text-textGreen text-sm tracking-wide'>
+              Featured Project
+            </p>
+            <h3 className='text-2xl font-bold'>Synethesia</h3>
+            <p className='bg-[#112240] text-sm md:text-base p-2 md:p-6 rounded-md'>
+              A music visualizer with 3D artwork utilizing <span className='text-textGreen'>Three.js</span> that is <span className='text-textGreen'>interactive with the music</span> being played.
+            </p>
+          </div>
+        </div>
       </div>
 
     </section>
