76139fa4cc9a5e131e4a04cc1b7590f843f5b86c
Kyle Crawford <kcraw217@gmail.com>
2023-09-13 22:46:11 -0700

    fix components folder name

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,15 @@
-import Navbar from "./comonents/Navbar";
+import Navbar from "./components/Navbar";
 
 
 export default function Home() {
   return (
     <main className="w-full h-screen font-bodyFont bg-bodyColor text-textLight overflow-x-hidden overflow-y-scroll">
       <Navbar />
+      <div className="w-full h-[99vh] xl:flex items-center gap-20 justify-between">
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
     </main>
   )
 }
