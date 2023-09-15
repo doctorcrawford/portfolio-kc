7955fa7791d5d768fe93af9319a377cb3fd8b4a2
Kyle Crawford <kcraw217@gmail.com>
2023-09-14 22:29:49 -0700

    fix colors for icons

--- pages/components/LeftSide.tsx
+++ pages/components/LeftSide.tsx
@@ -1,33 +1,71 @@
 import { TbBrandGithub } from 'react-icons/tb';
-import { SlSocialLinkedin, SlSocialSpotify, SlMusicToneAlt } from 'react-icons/sl'
+import { SlSocialLinkedin, SlSocialSpotify, SlMusicToneAlt } from 'react-icons/sl';
+import { motion } from 'framer-motion';
+
 
 const LeftSide = () => {
   return (
     <div className='w-full h-full flex flex-col items-center justify-end gap-4 text-textLight'>
-      <div className='flex flex-col gap-4'>
-        <a href="https://github.com/doctorcrawford" target='_blank'>
-          <span className='w-10 h-10 text-xl bg-hoverColor rounded-full inline-flex items-center justify-center hover:text-textYellow cursor-pointer hover:-translate-y-2 transition-all duration-300'>
-            <TbBrandGithub />
-            </span>
-        </a>
-        <a href="https://www.linkedin.com/in/kyle-crawford123/" target='_blank'>
-          <span className='w-10 h-10 text-xl bg-hoverColor rounded-full inline-flex items-center justify-center hover:text-textYellow cursor-pointer hover:-translate-y-2 transition-all duration-300'>
-            <SlSocialLinkedin />
-            </span>
-        </a>
-        <a href="https://open.spotify.com/artist/6IePE0TDlAmOQT7iZtlZ55?si=QdxoQNRgQMCTzVUv3PK4Iw" target='_blank'>
-          <span className='w-10 h-10 text-xl bg-hoverColor rounded-full inline-flex items-center justify-center hover:text-textYellow cursor-pointer hover:-translate-y-2 transition-all duration-300'>
-            <SlSocialSpotify />
-            </span>
-        </a>
-        <a href="https://linktr.ee/knxnmusic" target='_blank'>
-          <span className='w-10 h-10 text-xl bg-hoverColor rounded-full inline-flex items-center justify-center hover:text-textYellow cursor-pointer hover:-translate-y-2 transition-all duration-300'>
-            <SlMusicToneAlt />
-            </span>
-        </a>
-      </div>
-      <div className='w-[2px] h-32 bg-textDark'></div>
+    <div className='flex flex-col gap-4'>
+      <motion.a
+        initial={{ y: 20, opacity: 0 }}
+        animate={{ x: 0, opacity: 1 }}
+        transition={{
+          delay: 1,
+          ease: "easeIn"
+        }}
+        href="https://github.com/doctorcrawford"
+        target='_blank'
+      >
+        <span className='w-10 h-10 text-xl bg-bodyColor border-[1px] border-zinc-700 hover:border-textYellow text-zinc-200 rounded-full inline-flex items-center justify-center hover:text-textYellow cursor-pointer hover:-translate-y-2 transition-all duration-300'>
+          <TbBrandGithub />
+        </span>
+      </motion.a>
+      <motion.a
+        initial={{ y: 20, opacity: 0 }}
+        animate={{ x: 0, opacity: 1 }}
+        transition={{
+          delay: 1,
+          ease: "easeIn"
+        }}
+        href="https://www.linkedin.com/in/kyle-crawford123/"
+        target='_blank'
+      >
+        <span className='w-10 h-10 text-xl bg-bodyColor border-[1px] border-zinc-700 hover:border-textYellow text-zinc-200 rounded-full inline-flex items-center justify-center hover:text-textYellow cursor-pointer hover:-translate-y-2 transition-all duration-300'>
+          <SlSocialLinkedin />
+        </span>
+      </motion.a>
+      <motion.a
+        initial={{ y: 20, opacity: 0 }}
+        animate={{ x: 0, opacity: 1 }}
+        transition={{
+          delay: 1,
+          ease: "easeIn"
+        }}
+        href="https://open.spotify.com/artist/6IePE0TDlAmOQT7iZtlZ55?si=QdxoQNRgQMCTzVUv3PK4Iw"
+        target='_blank'
+      >
+        <span className='w-10 h-10 text-xl bg-bodyColor border-[1px] border-zinc-700 hover:border-textYellow text-zinc-200 rounded-full inline-flex items-center justify-center hover:text-textYellow cursor-pointer hover:-translate-y-2 transition-all duration-300'>
+          <SlSocialSpotify />
+        </span>
+      </motion.a>
+      <motion.a
+        initial={{ y: 20, opacity: 0 }}
+        animate={{ x: 0, opacity: 1 }}
+        transition={{
+          delay: 1,
+          ease: "easeIn"
+        }}
+        href="https://linktr.ee/knxnmusic"
+        target='_blank'
+      >
+        <span className='w-10 h-10 text-xl bg-bodyColor border-[1px] border-zinc-700 hover:border-textYellow text-zinc-200 rounded-full inline-flex items-center justify-center hover:text-textYellow cursor-pointer hover:-translate-y-2 transition-all duration-300'>
+          <SlMusicToneAlt />
+        </span>
+      </motion.a>
     </div>
+    <div className='w-[2px] h-32 bg-textDark'></div>
+  </div>
   );
 };
 
