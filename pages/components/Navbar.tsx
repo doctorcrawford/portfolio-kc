0b799ab97e59f25994c61db9c8c67a092ab39fc6
Kyle Crawford <kcraw217@gmail.com>
2023-09-14 22:30:06 -0700

    add icons to side menu

--- pages/components/Navbar.tsx
+++ pages/components/Navbar.tsx
@@ -4,6 +4,8 @@ import Link from 'next/link'
 import { motion } from 'framer-motion'
 import { useRef, useState } from "react";
 import { MdOutlineClose } from 'react-icons/md';
+import { TbBrandGithub } from 'react-icons/tb';
+import { SlSocialLinkedin, SlSocialSpotify, SlMusicToneAlt } from 'react-icons/sl'
 
 const Navbar = () => {
   const ref = useRef<string | any>("");
@@ -189,6 +191,65 @@ const Navbar = () => {
                       </motion.li>
                     </Link>
                   </ul>
+                  <div className='flex flex-row gap-4'>
+                    <motion.a
+                      initial={{ y: 20, opacity: 0 }}
+                      animate={{ x: 0, opacity: 1 }}
+                      transition={{
+                        delay: 1,
+                        ease: "easeIn"
+                      }}
+                      href="https://github.com/doctorcrawford"
+                      target='_blank'
+                    >
+                      <span className='w-10 h-10 text-xl bg-bodyColor border-[1px] border-zinc-700 hover:border-textYellow text-zinc-200 rounded-full inline-flex items-center justify-center hover:text-textYellow cursor-pointer hover:-translate-y-2 transition-all duration-300'>
+                        <TbBrandGithub />
+                      </span>
+                    </motion.a>
+                    <motion.a
+                      initial={{ y: 20, opacity: 0 }}
+                      animate={{ x: 0, opacity: 1 }}
+                      transition={{
+                        delay: 1,
+                        ease: "easeIn"
+                      }}
+                      href="https://www.linkedin.com/in/kyle-crawford123/"
+                      target='_blank'
+                    >
+                      <span className='w-10 h-10 text-xl bg-bodyColor border-[1px] border-zinc-700 hover:border-textYellow text-zinc-200 rounded-full inline-flex items-center justify-center hover:text-textYellow cursor-pointer hover:-translate-y-2 transition-all duration-300'>
+                        <SlSocialLinkedin />
+                      </span>
+                    </motion.a>
+                    <motion.a
+                      initial={{ y: 20, opacity: 0 }}
+                      animate={{ x: 0, opacity: 1 }}
+                      transition={{
+                        delay: 1,
+                        ease: "easeIn"
+                      }}
+                      href="https://open.spotify.com/artist/6IePE0TDlAmOQT7iZtlZ55?si=QdxoQNRgQMCTzVUv3PK4Iw"
+                      target='_blank'
+                    >
+                      <span className='w-10 h-10 text-xl bg-bodyColor border-[1px] border-zinc-700 hover:border-textYellow text-zinc-200 rounded-full inline-flex items-center justify-center hover:text-textYellow cursor-pointer hover:-translate-y-2 transition-all duration-300'>
+                        <SlSocialSpotify />
+                      </span>
+                    </motion.a>
+                    <motion.a
+                      initial={{ y: 20, opacity: 0 }}
+                      animate={{ x: 0, opacity: 1 }}
+                      transition={{
+                        delay: 1,
+                        ease: "easeIn"
+                      }}
+                      href="https://linktr.ee/knxnmusic"
+                      target='_blank'
+                    >
+                      <span className='w-10 h-10 text-xl bg-bodyColor border-[1px] border-zinc-700 hover:border-textYellow text-zinc-200 rounded-full inline-flex items-center justify-center hover:text-textYellow cursor-pointer hover:-translate-y-2 transition-all duration-300'>
+                        <SlMusicToneAlt />
+                      </span>
+                    </motion.a>
+                  </div>
+
                 </div>
               </motion.div>
             </div>
