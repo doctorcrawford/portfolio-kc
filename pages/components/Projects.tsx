69d2f78e488e3457c8c59a39d0d756ca6ad0b969
Kyle Crawford <kcraw217@gmail.com>
2023-09-15 08:52:51 -0700

    fix mouse mingle link

--- pages/components/Projects.tsx
+++ pages/components/Projects.tsx
@@ -61,7 +61,7 @@ const Projects = () => {
           <div className='flex flex-col xl:flex-row-reverse gap-6'>
             <a
               className='w-full xl:w-1/2 h-auto relative group'
-              href='https://doctorcrawford.github.io/synesthesia/'
+              href='https://github.com/doctorcrawford/MouseMingleMonoRepo'
               target='_blank'
             >
               <Image
