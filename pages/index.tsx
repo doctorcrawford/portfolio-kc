21ed5e50eb82c8d0e7fad8541fe0acae7511f5c3
Kyle Crawford <kcraw217@gmail.com>
2023-09-25 17:40:36 -0400

    add animation for middle section except banner

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,6 +14,7 @@ export default function Home() {
     <main className="w-full h-screen font-bodyFont bg-bodyColor text-textLight overflow-x-hidden overflow-y-scroll scrollbar scrollbar-track-textDark/20 scrollbar-thumb-textDark/60">
       <Navbar />
       <div className="w-full h-[88vh] flex flex-row xl:flex items-center gap-20 justify-between">
+
         <motion.div
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
@@ -21,14 +22,23 @@ export default function Home() {
           className="hidden xl:inline-flex w-32 h-full fixed left-0 bottom-0">
           <LeftSide />
         </motion.div>
+
         <div className="h-[88vh] w-full mx-auto px-4">
           <Banner />
-          <About />
-          <Projects />
-          <Archive />
-          <Contact />
-          <Footer />
+
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 1.5 }}
+          >
+            <About />
+            <Projects />
+            <Archive />
+            <Contact />
+            <Footer />
+          </motion.div>
         </div>
+
         <motion.div
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
