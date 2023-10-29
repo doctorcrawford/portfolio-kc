db18007be5982b903f36428d06d0fbe540652890
Kyle Crawford <kcraw217@gmail.com>
2023-10-29 12:42:43 -0700

    edit contact description

--- pages/components/Contact.tsx
+++ pages/components/Contact.tsx
@@ -12,7 +12,7 @@ const Contact = () => {
       </p>
       <h2 className="font-titleFont text-5xl font-semibold">Get In Touch</h2>
       <p className="max-w-[600px] text-center text-textDark">
-        Always looking for new challenges to tackle that allow me to apply my skills and experience while continuing to learn and grow.
+        Always looking for new challenges to tackle that allow me to apply my skills and experience while continuing to learn and grow. Or just say hello!
       </p>
       <a href="mailto:noor.kcraw217@gmail.com">
         <button className="w-40 h-14 border border-textYellow mt-6 font-titleFont text-sm text-textGreen tracking-wider rounded-md hover:bg-hoverColor duration-300">
