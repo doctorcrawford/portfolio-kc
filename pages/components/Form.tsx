1fb94f0780045b2aa0900cf5d5c91432f2630ffc
Kyle Crawford <kcraw217@gmail.com>
2023-11-04 12:58:53 -0700

    WIP form submission

--- pages/components/Form.tsx
+++ pages/components/Form.tsx
@@ -1,89 +1,101 @@
 import { sendForm } from "@emailjs/browser";
+import { Input } from "postcss";
 import { useState } from "react"
 
 const Form = () => {
-  // const initialValues = {
-  //   name: '',
-  //   email: '',
-  //   message: ''
-  // }
+  const initialValues = {
+    name: '',
+    email: '',
+    subject: '',
+    message: ''
+  }
 
-  // const [formValues, setFormValues] = useState(initialValues);
-  // const [formSubmit, setFormSubmit] = useState(false);
-  // const [alert, setAlert] = useState(false);
+  const [formValues, setFormValues] = useState(initialValues);
+  const [formSubmit, setFormSubmit] = useState(false);
+  const [alert, setAlert] = useState(false);
 
-  // const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-  //   const { name, value } = e.target;
-  //   setFormValues({
-  //     ...formValues,
-  //     [name]: value
-  //   });
-  // };
+  type InputText = React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLTextAreaElement>
 
-  // const sendEmail = (e) => {
-  //   e.preventDefault();
-  //   const serviceID = "service_kzitiuc";
-  //   const templateID = "template_x6q03hw";
-  //   const apiKey = process.env.EMAILJS_PUBLIC_KEY;
+  const handleInputChange = (e: InputText) => {
+    const { name, value } = e.target;
+    setFormValues({
+      ...formValues,
+      [name]: value
+    });
+  };
 
-  //   sendForm(serviceID, templateID, e.target, apiKey)
-  //     .then((result) => {
-  //       setAlert(false);
-  //       setFormValues(initialValues);
-  //       setFormSubmit(true);
-  //     },
-  //       (error) => {
-  //         console.log(error.text)
-  //       });
-  // };
+  const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const serviceID = "service_kzitiuc";
+    const templateID = "template_x6q03hw";
+    const apiKey = process.env.EMAILJS_PUBLIC_KEY;
+    const target = e.target as HTMLFormElement
+
+    sendForm(serviceID, templateID, target, apiKey)
+      .then((result) => {
+        setAlert(false);
+        setFormValues(initialValues);
+        setFormSubmit(true);
+      },
+        (error) => {
+          console.log(error.text)
+        });
+  };
+
+  const validateFormFields = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (formValues.name === '' || formValues.email === '' || formValues.message === '') {
+      setAlert(true)
+    } else {
+      sendEmail(e);
+    }
+  };
 
   return (
     <form
-      id="myForm"
+      onSubmit={validateFormFields}
       className='space-y-2 text-black w-11/12'
     >
-      {/* <!-- name --> */}
-      <div className="form-group">
-        <input
-          type="name"
-          name="name"
-          className="form-control rounded-md w-full p-1"
-          id="name"
-          placeholder="Name"
-        />
-      </div>
+      <input
+        type="text"
+        name="name"
+        className="form-control rounded-md w-full p-1"
+        id="name"
+        placeholder="Name"
+        value={formValues.name}
+        onChange={handleInputChange}
+      />
 
-      {/* <!-- email --> */}
-      <div className="form-group">
-        <input
-          type="email"
-          name="email"
-          className="form-control rounded-md w-full p-1"
-          id="email"
-          placeholder="Email Address"
-        />
-      </div>
+      <input
+        type="email"
+        name="email"
+        className="form-control rounded-md w-full p-1"
+        id="email"
+        placeholder="Email Address"
+        value={formValues.email}
+        onChange={handleInputChange}
+      />
 
-      {/* <!-- subject --> */}
-      <div className="form-group">
-        <input
-          type="text"
-          name="subject"
-          className=" rounded-md p-1 xl:p-2 w-full"
-          id="subject"
-          placeholder="Subject"
-        />
-      </div>
+      <input
+        type="text"
+        name="subject"
+        className=" rounded-md p-1 xl:p-2 w-full"
+        id="subject"
+        placeholder="Subject"
+        value={formValues.subject}
+        onChange={handleInputChange}
+      />
 
-      <div className="form-group">
-        <textarea
-          className="form-control rounded-md w-full p-1"
-          id="message"
-          name="message"
-          rows={5}
-          placeholder='Message'
-        ></textarea>
-      </div>
+      <textarea
+        className="form-control rounded-md w-full p-1"
+        id="message"
+        name="message"
+        rows={5}
+        placeholder='Message'
+        value={formValues.message}
+        onChange={handleInputChange}
+      ></textarea>
 
       <button
         type="submit"
