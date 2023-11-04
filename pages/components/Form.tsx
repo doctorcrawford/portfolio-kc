fb96f841ceec896737ddafaefd3e371607455d73
Kyle Crawford <kcraw217@gmail.com>
2023-11-04 14:46:31 -0700

    configure form to send email

--- pages/components/Form.tsx
+++ pages/components/Form.tsx
@@ -28,8 +28,9 @@ const Form = () => {
     e.preventDefault();
     const serviceID = "service_kzitiuc";
     const templateID = "template_x6q03hw";
-    const apiKey = process.env.EMAILJS_PUBLIC_KEY;
+    const apiKey = process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY
     const target = e.target as HTMLFormElement
+    console.log(apiKey)
 
     sendForm(serviceID, templateID, target, apiKey)
       .then((result) => {
@@ -53,57 +54,69 @@ const Form = () => {
   };
 
   return (
-    <form
-      onSubmit={validateFormFields}
-      className='space-y-2 text-black w-11/12'
-    >
-      <input
-        type="text"
-        name="name"
-        className="form-control rounded-md w-full p-1"
-        id="name"
-        placeholder="Name"
-        value={formValues.name}
-        onChange={handleInputChange}
-      />
+    <>
+      {alert ?
+        <p className="text-red-500">
+          *Please complete all form fields before submitting
+        </p> :
+        null
+      }
+      {formSubmit ? null :
+        <form
+          id='contactForm'
+          method='POST'
+          onSubmit={validateFormFields}
+          className='space-y-2 text-black w-11/12'
+        >
+          <input
+            type="text"
+            name="name"
+            className="form-control rounded-md w-full p-1"
+            id="name"
+            placeholder="Name"
+            value={formValues.name}
+            onChange={handleInputChange}
+          />
 
-      <input
-        type="email"
-        name="email"
-        className="form-control rounded-md w-full p-1"
-        id="email"
-        placeholder="Email Address"
-        value={formValues.email}
-        onChange={handleInputChange}
-      />
+          <input
+            type="email"
+            name="email"
+            className="form-control rounded-md w-full p-1"
+            id="email"
+            placeholder="Email Address"
+            value={formValues.email}
+            onChange={handleInputChange}
+          />
 
-      <input
-        type="text"
-        name="subject"
-        className=" rounded-md p-1 xl:p-2 w-full"
-        id="subject"
-        placeholder="Subject"
-        value={formValues.subject}
-        onChange={handleInputChange}
-      />
+          <input
+            type="text"
+            name="subject"
+            className=" rounded-md p-1 xl:p-2 w-full"
+            id="subject"
+            placeholder="Subject"
+            value={formValues.subject}
+            onChange={handleInputChange}
+          />
 
-      <textarea
-        className="form-control rounded-md w-full p-1"
-        id="message"
-        name="message"
-        rows={5}
-        placeholder='Message'
-        value={formValues.message}
-        onChange={handleInputChange}
-      ></textarea>
+          <textarea
+            className="form-control rounded-md w-full p-1"
+            id="message"
+            name="message"
+            rows={5}
+            placeholder='Message'
+            value={formValues.message}
+            onChange={handleInputChange}
+          ></textarea>
 
-      <button
-        type="submit"
-        className="bg-textYellow p-1.5 rounded-md hover:text-white xl:p-2"
-      >
-        Submit
-      </button>
-    </form>
+          <button
+            type="submit"
+            className="bg-textYellow p-1.5 rounded-md hover:text-white xl:p-2"
+          >
+            Submit
+          </button>
+        </form>
+      }
+    </>
   );
 }
 
